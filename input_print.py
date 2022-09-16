38a879c0ac63aba7168ca8dd233d263ddeb2b6a9
Rahul Rao <nkrahul88@gmail.com>
2022-09-16 15:28:36 +0100

    Introduction to String concatenantion and variables

---
+++ input_print.py
@@ -0,0 +1,5 @@
+#!/bin/python
+name=input('What is your name?')
+print(len(name))
+
+print( len( input("What is your name?") ) )
---
+++ variables.py
@@ -0,0 +1,9 @@
+a = input("a: ")
+b = input("b: ")
+
+c = a
+d = b
+
+
+print("a: " + c)
+print("b: " + d)
